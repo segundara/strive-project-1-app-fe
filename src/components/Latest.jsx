7f8f318c263dc3108abfbe78ebb3054d16c3ce7c
segundara <segundara@gmail.com>
2020-07-01 11:11:45 +0300

    update to latest js

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -32,7 +32,7 @@ class LatestRealease extends React.Component {
             let books = await response.json();
             console.log(books)
             this.setState({
-                books: books.data,
+                books: books.filteredBooks,
                 loading: false
             });
             } catch (err) {
@@ -56,6 +56,7 @@ class LatestRealease extends React.Component {
                 method: "GET",
             });
             let books = await response.json();
+            console.log(books)
             this.setState({
                 books: books.data,
                 loading: false
@@ -73,8 +74,8 @@ class LatestRealease extends React.Component {
 
 
     render() {
-        console.log(this.state.categorySelected)
-        console.log(this.state.books)
+        // console.log(this.state.categorySelected)
+        // console.log(this.state.books)
         return (
             <>
                 <Row className="mx-0 pb-3">
