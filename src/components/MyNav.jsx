731cfc7af7163f9b628ebdd66c706d2cb12d5052
segundara <segundara@gmail.com>
2020-08-30 10:13:38 +0300

    update to myNav and Welcome jsx

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,37 +1,30 @@
 import React from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link, withRouter } from 'react-router-dom';
 
 class NavBar extends React.Component {
     render() {
         return (
             <div className="fixed-top">
-            <Navbar collapseOnSelect expand="lg" variant="dark" style={{backgroundColor: '#343a40', border: 2 + 'px solid #6351ce'}}>
-                <Link to='/'>
-                    <Navbar.Brand>Strive BookStore</Navbar.Brand>
-                </Link>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Link to="/"
-                            className={
-                                this.props.location.pathname === '/'
-                                ? "nav-link active"
-                                : "nav-link"
-                            }
+                <Navbar collapseOnSelect expand="lg" variant="dark" style={{ backgroundColor: '#343a40', border: 2 + 'px solid #6351ce' }}>
+                    <Link to='/'>
+                        <Navbar.Brand>Strive BookStore</Navbar.Brand>
+                    </Link>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Link to="/"
+                                className={
+                                    this.props.location.pathname === '/'
+                                        ? "nav-link active"
+                                        : "nav-link"
+                                }
                             >
-                            Home
+                                Home
                         </Link>
-                        <Nav.Link href="#">About</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#">More details</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </div>
         )
     }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Jumbotron, Container} from 'react-bootstrap';
+import { Jumbotron, Container } from 'react-bootstrap';
 
 class JumBotron extends React.Component {
     render() {
         return (
-            <Jumbotron fluid className="text-center">
+            <Jumbotron fluid className="text-center pt-5">
                 <Container>
                     <h1>Welcome!</h1>
                     <p>
-                        If you have been searching for the right place 
-                        to get the books that would enrich your info-bank, 
+                        If you have been searching for the right place
+                        to get the books that would enrich your info-bank,
                         then you are just in the right place.
+                    </p>
+                    <p>
+                        Click on the dropdown button to select and see the books in each category.
                     </p>
                 </Container>
             </Jumbotron>
